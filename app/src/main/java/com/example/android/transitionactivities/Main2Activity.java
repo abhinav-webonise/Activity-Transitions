48c78b97a79cd288461083e845fe8fa649c7af32
Abhinav Bahiya <abhinav.bahiya@weboniselab.com>
2016-12-29 18:19:59 +0530

    TAG added

--- app/src/main/java/com/example/android/transitionactivities/Main2Activity.java
+++ app/src/main/java/com/example/android/transitionactivities/Main2Activity.java
@@ -3,15 +3,18 @@ package com.example.android.transitionactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
     private Button btnActivity2;
+    final String TAG = getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Log.d(TAG, "the onCreate() 2 Event called");
         btnActivity2 = (Button) findViewById(R.id.button2);
         backToActivity1();
     }
@@ -24,5 +27,35 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         Intent i = new Intent(Main2Activity.this, MainActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "thr onStart() 2 event called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "The onResume() 2 event called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "The onPause() 2 event called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "the onStop() 2 event called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "the onDestroy() 2 event was called");
     }
 }
--- app/src/main/java/com/example/android/transitionactivities/MainActivity.java
+++ app/src/main/java/com/example/android/transitionactivities/MainActivity.java
@@ -1,31 +1,72 @@
 package com.example.android.transitionactivities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    // 1 - Declare Button Type
     private Button btnActivity1;
+    final String TAG = getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnActivity1 = (Button) findViewById(R.id.button1);
+        Log.d(TAG, "the onCreate() Event called");  // Activity Life Cycle Method
 
+        // 2 - Find View By ID
+        btnActivity1 = (Button) findViewById(R.id.button1);
+        // 3- Call Function
         goToActivity1();
     }
 
     private void goToActivity1() {
+        // 4 - Set On Click Listener Event
         btnActivity1.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        // 5 - Intent and Start
         Intent i = new Intent(MainActivity.this, Main2Activity.class);
         startActivity(i);
     }
+    // ----------------------------------------------------------------------------------
+    // Life Cycle Methods
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "thr onStart() event called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "The onResume() event called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "The onPause() event called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "the onStop() event called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "the onDestroy event was called");
+    }
+    //--------------------------------------------------------------------------------------
 }
+
+
